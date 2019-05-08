abf4fc648864a05e99be3b18fc5d0ee32fcb341f
Bob Hall <myk_quayce@hotmail.com>
2019-05-08 17:28:43 +0100

    rtsftfstsfr

--- MovieTimes.CineworldService/build.sh
+++ MovieTimes.CineworldService/build.sh
@@ -1,18 +1,12 @@
 #! /bin/bash
 
-docker image ls | tail --line +2 | grep 'mcr.microsoft.com/dotnet/core' | awk '{system("docker pull " $1 ":" $2)}'
+docker image ls | tail --line +2 | grep 'mcr.microsoft.com/dotnet/core/' | grep --invert-match '<none>' | awk '{system("docker pull " $1 ":" $2)}'
 
 if [ $? -ne 0 ]; then
     exit 1
 fi
 
 docker build --tag eassbhhtgu/movietimes-cineworldservice:latest .
-
-if [ $? -ne 0 ]; then
-    exit 1
-fi
-
-docker image ls | tail --line +2 | grep none | awk '{system("docker rmi " $3)}'
 
 if [ $? -ne 0 ]; then
     exit 1
