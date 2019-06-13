088f8bc781d62227bb9cbb3a00f0abdb30a4b8b3
Bob Hall <myk_quayce@hotmail.com>
2019-06-13 04:08:06 +0100

    usings

--- MovieTimes.CineworldService/MovieTimes.CineworldService.Repositories/Concrete/CineworldRepository.cs
+++ MovieTimes.CineworldService/MovieTimes.CineworldService.Repositories/Concrete/CineworldRepository.cs
@@ -30,76 +30,73 @@ namespace MovieTimes.CineworldService.Repositories.Concrete
 		{
 			var (cinemaCount, filmCount, showCount) = cinemas.GetCounts();
 
-			using (var scope = _tracer.BuildDefaultSpan()
+			using var scope = _tracer.BuildDefaultSpan()
 				.WithTag(nameof(cinemaCount), cinemaCount)
 				.WithTag(nameof(filmCount), filmCount)
 				.WithTag(nameof(showCount), showCount)
-				.StartActive(finishSpanOnDispose: true))
+				.StartActive(finishSpanOnDispose: true);
+
+			await ExecuteAsync("SELECT NOW();");
+
+			_logger.LogInformation("Saving {0:D} {1}(s), {2:D} {3}(s), {4:D} {5}(s)", cinemaCount, nameof(cinema), filmCount, nameof(film), showCount, nameof(show));
+
+			using var transaction = BeginTransaction();
+
+			try
 			{
+				await ExecuteAsync("DELETE FROM cineworld.show WHERE cinemaId>=0", transaction: transaction);
 
-				await ExecuteAsync("SELECT NOW();");
+				await Task.WhenAll(
+					ExecuteAsync("DELETE FROM cineworld.film WHERE edi>=0", transaction: transaction),
+					ExecuteAsync("DELETE FROM cineworld.cinema WHERE id>=0", transaction: transaction)
+					);
 
-				_logger.LogInformation("Saving {0:D} {1}(s), {2:D} {3}(s), {4:D} {5}(s)", cinemaCount, nameof(cinema), filmCount, nameof(film), showCount, nameof(show));
+				await Task.WhenAll(
+					ExecuteAsync(
+						"INSERT cineworld.cinema(id, name) VALUES (@id, @name)",
+						from c in cinemas.cinema
+						select new
+						{
+							c.id,
+							c.name,
+						},
+						transaction: transaction),
+					ExecuteAsync(
+						"INSERT cineworld.film(edi, title) VALUES (@edi, @title)",
+						from c in cinemas.cinema
+						from f in c.listing
+						where f.edi > 0 // Theatre Let
+							group f by f.edi into gg
+						select new
+						{
+							edi = gg.Key,
+							gg.First().title,
+						},
+						transaction: transaction)
+					);
 
-				using (var transaction = BeginTransaction())
-				{
-					try
+				await ExecuteAsync(
+					"INSERT cineworld.show(cinemaId, filmEdi, time) VALUES (@cinemaId, @filmEdi, @time)",
+					from c in cinemas.cinema
+					from f in c.listing
+					where f.edi > 0 // Theatre Let
+						from s in f.shows
+					group (c, f, s) by (c.id, f.edi, s.time) into gg
+					select new
 					{
-						await ExecuteAsync("DELETE FROM cineworld.show WHERE cinemaId>=0", transaction: transaction);
+						cinemaId = gg.Key.id,
+						filmEdi = gg.Key.edi,
+						gg.Key.time,
+					},
+					transaction: transaction);
 
-						await Task.WhenAll(
-							ExecuteAsync("DELETE FROM cineworld.film WHERE edi>=0", transaction: transaction),
-							ExecuteAsync("DELETE FROM cineworld.cinema WHERE id>=0", transaction: transaction)
-							);
-
-						await Task.WhenAll(
-							ExecuteAsync(
-								"INSERT cineworld.cinema(id, name) VALUES (@id, @name)",
-								from c in cinemas.cinema
-								select new
-								{
-									c.id,
-									c.name,
-								},
-								transaction: transaction),
-							ExecuteAsync(
-								"INSERT cineworld.film(edi, title) VALUES (@edi, @title)",
-								from c in cinemas.cinema
-								from f in c.listing
-								where f.edi > 0 // Theatre Let
-								group f by f.edi into gg
-								select new
-								{
-									edi = gg.Key,
-									gg.First().title,
-								},
-								transaction: transaction)
-							);
-
-						await ExecuteAsync(
-							"INSERT cineworld.show(cinemaId, filmEdi, time) VALUES (@cinemaId, @filmEdi, @time)",
-							from c in cinemas.cinema
-							from f in c.listing
-							where f.edi > 0 // Theatre Let
-							from s in f.shows
-							group (c, f, s) by (c.id, f.edi, s.time) into gg
-							select new
-							{
-								cinemaId = gg.Key.id,
-								filmEdi = gg.Key.edi,
-								gg.Key.time,
-							},
-							transaction: transaction);
-
-						transaction.Commit();
-					}
-					catch (MySqlException ex)
-					{
-						scope.Span.Log(nameof(ex), ex.ToJsonString());
-						_logger.LogCritical(ex, "Error saving cinemas");
-						transaction.Rollback();
-					}
-				}
+				transaction.Commit();
+			}
+			catch (MySqlException ex)
+			{
+				scope.Span.Log(nameof(ex), ex.ToJsonString());
+				_logger.LogCritical(ex, "Error saving cinemas");
+				transaction.Rollback();
 			}
 		}
 	}
