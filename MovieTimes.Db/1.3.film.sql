bc42d39c34af28401b75eb78dcebadaac054c0f0
Bob Hall <myk_quayce@hotmail.com>
2019-06-14 23:28:23 +0100

    adding IMDB ID to cineword.film table

--- MovieTimes.Db/1.3.film.sql
+++ MovieTimes.Db/1.3.film.sql
@@ -1,6 +1,7 @@
 CREATE TABLE `cineworld`.`film` (
   `edi` INT UNSIGNED NOT NULL,
   `title` VARCHAR(100) NOT NULL,
+  `imdbId` INT UNSIGNED NULL,
   PRIMARY KEY (`edi`),
   UNIQUE INDEX `uix_cineworld_film` (`title`)
 );
