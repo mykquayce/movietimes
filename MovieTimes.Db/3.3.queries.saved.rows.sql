bd6819944c8f15e1773eb2efc56cb1cdff2e6836
Bob Hall <myk_quayce@hotmail.com>
2019-07-01 12:48:43 +0100

    adding another saved query

--- MovieTimes.Db/3.3.queries.saved.rows.sql
+++ MovieTimes.Db/3.3.queries.saved.rows.sql
@@ -1,2 +1,4 @@
 INSERT `queries`.`saved` (`query`)
-VALUES ('/v1/shows?cinema=ashton&cinema=nec&cinema=solihull&dayofweek=friday&timeofday=evening&timeofday=night&weekCount=1');
+VALUES
+	('/v1/shows?cinema=ashton&cinema=nec&cinema=solihull&dayofweek=friday&timeofday=evening&timeofday=night&weekCount=1'),
+	('/v1/shows?cinema=ashton&cinema=nec&cinema=solihull&title=preview&title=unlimited')
