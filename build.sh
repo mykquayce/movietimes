ff86da8c4cac05597cd278aebdfd6ec784c67f6d
Bob Hall <myk_quayce@hotmail.com>
2019-11-01 02:09:48 +0000

    build.sh now creates the docker secret, if it's missing

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 #! /bin/bash
 
+docker secret ls | tail --line +2 | grep 'MySqlCineworldPassword'
+
+if [ $? -ne 0 ]; then
+    openssl rand -base64 201 | sed 's/[^0-9A-Za-z]//g' | sed -z 's/\n//g' | docker secret create MySqlCineworldPassword -
+fi
+
 docker pull jaegertracing/all-in-one:latest
 docker pull mariadb:latest
 docker pull eassbhhtgu/movietimes-api:latest
