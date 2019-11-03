e805bde5439bce482a8defc2d471663a43641d2f
Bob Hall <myk_quayce@hotmail.com>
2019-11-03 20:25:36 +0000

    adding MovieTimes.Service.WorkerService

---
+++ MovieTimes.Service/MovieTimes.Service.WorkerService/Program.cs
@@ -0,0 +1,83 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace MovieTimes.Service.WorkerService
+{
+	public class Program
+	{
+		public static Task Main(string[] args)
+		{
+			var hostBuilder = Host.CreateDefaultBuilder(args);
+
+			hostBuilder
+				.ConfigureServices((hostContext, services) =>
+				{
+					// http clients
+					services
+						.AddHttpClient(
+							nameof(Clients.Concrete.ApiClient),
+							(_, client) =>
+							{
+								var uriSettings = hostContext.Configuration
+									.GetSection(nameof(Configuration.Uris))
+									.Get<Configuration.Uris>();
+
+								client.Timeout = TimeSpan.FromSeconds(1);
+								client.BaseAddress = new Uri(uriSettings.ApiBaseUri!, UriKind.Absolute);
+								client.DefaultRequestHeaders.Accept.Clear();
+								client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+							})
+						.ConfigurePrimaryHttpMessageHandler(() =>
+						{
+							return new HttpClientHandler { AllowAutoRedirect = false, };
+						});
+
+					// config
+					services
+						.Configure<Helpers.MySql.Models.DbSettings>(hostContext.Configuration.GetSection(nameof(Helpers.MySql.Models.DbSettings)))
+						.Configure<Configuration.Uris>(hostContext.Configuration.GetSection(nameof(Configuration.Uris)));
+
+					// clients
+					services
+						.AddTransient<Clients.IApiClient, Clients.Concrete.ApiClient>()
+						.AddTransient<Helpers.Cineworld.ICineworldClient, Helpers.Cineworld.Concrete.CineworldClient>();
+
+					// repos
+					var dbSettings = hostContext.Configuration
+						.GetSection(nameof(Helpers.MySql.Models.DbSettings))
+						.Get<Helpers.MySql.Models.DbSettings>();
+
+					services
+						.AddTransient<Repositories.ICineworldRepository, Repositories.Concrete.CineworldRepository>()
+						.AddTransient<Repositories.IQueriesRepository, Repositories.Concrete.QueriesRepository>()
+						.AddTransient<Repositories.IMovieDetailsRepository, Repositories.Concrete.MovieDetailsRepository>();
+
+					// services
+					services
+						.AddTransient<Repositories.IMovieDetailsRepository, Repositories.Concrete.MovieDetailsRepository>();
+
+					// steps
+					services
+						.AddTransient<Steps.GetLatestLogEntryStep>()
+						.AddTransient<Steps.GetListingsHeadersStep>()
+						.AddTransient<Steps.GetListingsStep>()
+						.AddTransient<Steps.GetQueriesStep>()
+						.AddTransient<Steps.RunQueryStep>()
+						.AddTransient<Steps.SaveCinemasStep>()
+						.AddTransient<Steps.SaveLogEntryStep>();
+
+					services
+						.AddWorkflow()
+						.AddHostedService<Worker>();
+				});
+
+			return hostBuilder
+				.RunConsoleAsync();
+		}
+	}
+}
---
+++ MovieTimes.Service/MovieTimes.Service.WorkerService/Worker.cs
@@ -0,0 +1,45 @@
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using WorkflowCore.Interface;
+
+namespace MovieTimes.Service.WorkerService
+{
+	public class Worker : BackgroundService
+	{
+		private const int _delay = 60 * 60 * 1_000;
+
+		private readonly ILogger<Worker> _logger;
+		private readonly IWorkflowHost _workflowHost;
+
+		public Worker(
+			ILogger<Worker> logger,
+			IWorkflowHost workflowHost)
+		{
+			_logger = logger;
+
+			_workflowHost = workflowHost;
+			_workflowHost.RegisterWorkflow<Workflows.Workflow, Models.PersistenceData>();
+			_workflowHost.OnStepError += WorkflowHost_OnStepError;
+		}
+
+		private void WorkflowHost_OnStepError(WorkflowCore.Models.WorkflowInstance workflow, WorkflowCore.Models.WorkflowStep step, Exception exception)
+		{
+			throw new NotImplementedException();
+		}
+
+		protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+		{
+			var persistenceData = new Models.PersistenceData();
+
+			while (!stoppingToken.IsCancellationRequested)
+			{
+				await _workflowHost.StartWorkflow(workflowId: nameof(Workflows.Workflow), data: persistenceData);
+
+				await Task.Delay(_delay, stoppingToken);
+			}
+		}
+	}
+}
