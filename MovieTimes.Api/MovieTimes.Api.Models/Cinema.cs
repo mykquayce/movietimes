8a49058d947c2218519c1e6ae8cb405256438670
Bob Hall <myk_quayce@hotmail.com>
2019-11-08 01:56:54 +0000

    adding Cinema, Movie, and Show structs

---
+++ MovieTimes.Api/MovieTimes.Api.Models/Cinema.cs
@@ -0,0 +1,24 @@
+﻿using Dawn;
+
+namespace MovieTimes.Api.Models
+{
+	public readonly struct Cinema
+	{
+		public Cinema(short id, string name)
+		{
+			Id = Guard.Argument(() => id).Positive().Value;
+			Name = Guard.Argument(() => name).NotNull().NotEmpty().NotWhiteSpace().Value;
+		}
+
+		public short Id { get; }
+
+		public string Name { get; }
+
+		#region Equality
+		public override bool Equals(object obj) => this == obj as Cinema?;
+		public override int GetHashCode() => Id.GetHashCode();
+		public static bool operator ==(Cinema left, Cinema right) => left.GetHashCode() == right.GetHashCode();
+		public static bool operator !=(Cinema left, Cinema right) => !(left == right);
+		#endregion Equality
+	}
+}
---
+++ MovieTimes.Api/MovieTimes.Api.Models/Movie.cs
@@ -0,0 +1,23 @@
+﻿using Dawn;
+
+namespace MovieTimes.Api.Models
+{
+	public readonly struct Movie
+	{
+		public Movie(string title, short duration)
+		{
+			Title = Guard.Argument(() => title).NotNull().NotEmpty().NotWhiteSpace().Value;
+			Duration = Guard.Argument(() => duration).Positive().Value;
+		}
+
+		public string Title { get; }
+		public short Duration { get; }
+
+		#region Equality
+		public override bool Equals(object obj) => this == obj as Movie?;
+		public override int GetHashCode() => Title.GetHashCode();
+		public static bool operator ==(Movie left, Movie right) => left.GetHashCode() == right.GetHashCode();
+		public static bool operator !=(Movie left, Movie right) => !(left == right);
+		#endregion Equality
+	}
+}
